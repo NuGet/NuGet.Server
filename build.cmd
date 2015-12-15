fa4ae333c6e8e60fb5adba3405c2fd57daf3d529
Maarten Balliauw <maarten@maartenballiauw.be>
2015-12-15 20:47:50 +0100

    xunit 2.0.0 -> 2.1.0

--- build.cmd
+++ build.cmd
@@ -29,8 +29,8 @@ IF %ERRORLEVEL% NEQ 0 goto error
 
 
 REM Test
-call :ExecuteCmd tools\nuget.exe install xunit.runner.console -Version 2.0.0 -OutputDirectory packages
-call :ExecuteCmd packages\xunit.runner.console.2.0.0\tools\xunit.console.exe test\NuGet.Server.Tests\bin\%config%\NuGet.Server.Tests.dll
+call :ExecuteCmd tools\nuget.exe install xunit.runner.console -Version 2.1.0 -OutputDirectory packages
+call :ExecuteCmd packages\xunit.runner.console.2.1.0\tools\xunit.console.exe test\NuGet.Server.Tests\bin\%config%\NuGet.Server.Tests.dll
 IF %ERRORLEVEL% NEQ 0 goto error
 
 
