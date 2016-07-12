496896706fd68f860cfe478093eb493ceb4a3bf6
Anders Ulveseth <anders@ulveseth.net>
2016-07-12 20:32:59 +0200

    Controller configuration moves json formatter to after xml formatter

--- src/NuGet.Server.V2/OData/NuGetODataControllerConfigurationAttribute.cs
+++ src/NuGet.Server.V2/OData/NuGetODataControllerConfigurationAttribute.cs
@@ -2,22 +2,50 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
 
 using System;
+using System.Linq;
 using System.Web.Http.Controllers;
 using System.Web.Http.OData.Formatter;
 using System.Web.Http.OData.Formatter.Deserialization;
 using NuGet.Server.V2.OData.Serializers;
+using System.Collections.Generic;
 
 namespace NuGet.Server.V2.OData
 {
     class NuGetODataControllerConfigurationAttribute : Attribute, IControllerConfiguration
     {
+        private static IList<ODataMediaTypeFormatter> _formatters;
+        private static object _syncLock = new object();
+
+        private IList<ODataMediaTypeFormatter> GetFormatters()
+        {
+            if (_formatters == null)
+            {
+                lock (_syncLock)
+                {
+                    if (_formatters == null)
+                    {
+                        var serProvider = new CustomSerializerProvider(provider => new NuGetEntityTypeSerializer(provider));
+                        var createdFormatters = ODataMediaTypeFormatters.Create(serProvider, new DefaultODataDeserializerProvider());
+
+                        var jsonFormatters = createdFormatters.Where(x => x.SupportedMediaTypes.Any(y => y.MediaType.Contains("json")));
+                        createdFormatters.RemoveAll(x => jsonFormatters.Contains(x));
+                        var xmlFormatterIndex = createdFormatters.IndexOf(createdFormatters.Last(x => x.SupportedMediaTypes.Any(y => y.MediaType.Contains("xml"))));
+                        foreach(var formatter in jsonFormatters)
+                        {
+                            createdFormatters.Insert(xmlFormatterIndex++, formatter);
+                        }
+                        _formatters = createdFormatters;                        
+                    }
+                }
+            }
+            return _formatters;
+        }
+
+
         public void Initialize(HttpControllerSettings controllerSettings, HttpControllerDescriptor controllerDescriptor)
         {
-            var serProvider = new CustomSerializerProvider(provider => new NuGetEntityTypeSerializer(provider));
-            var formatters = ODataMediaTypeFormatters.Create(serProvider, new DefaultODataDeserializerProvider());
-
             controllerSettings.Formatters.Clear();
-            controllerSettings.Formatters.InsertRange(0, formatters);
+            controllerSettings.Formatters.InsertRange(0, GetFormatters());
         }
     }
 }
