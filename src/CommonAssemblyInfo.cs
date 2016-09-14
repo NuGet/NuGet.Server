5fca734365c7b3cd9c2bcf1847b296ceb8b024da
Maarten Balliauw <maarten@maartenballiauw.be>
2016-09-14 10:27:01 +0200

    Add AssemblyInformationalVersion so we can automate prerelease depenencies

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -14,5 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.14.0.0")]
 [assembly: AssemblyFileVersion("2.14.0.0")]
+[assembly: AssemblyInformationalVersion("2.14.0.0")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
