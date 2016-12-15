4f1bd9f28f4bd04c4d6d4d3894d141e558273600
Joel Verhagen <jver@microsoft.com>
2016-12-14 17:47:15 -0800

    Bump dependency versions and bump output version to 2.14.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.11.3.0")]
-[assembly: AssemblyFileVersion("2.11.3.0")]
+[assembly: AssemblyVersion("2.14.0.0")]
+[assembly: AssemblyFileVersion("2.14.0.0")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
