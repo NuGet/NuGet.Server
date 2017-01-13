f7c8247857e897c07a69e0ed0f27c9a8edd94baf
Joel Verhagen <jver@microsoft.com>
2017-01-13 09:40:56 -0800

    Address comment

--- src/NuGet.Server/Infrastructure/WebConfigSettingsProvider.cs
+++ src/NuGet.Server/Infrastructure/WebConfigSettingsProvider.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Specialized;
 using System.Web.Configuration;
 using NuGet.Server.Core.Infrastructure;
@@ -9,21 +10,23 @@ namespace NuGet.Server.Infrastructure
 {
     public class WebConfigSettingsProvider : ISettingsProvider
     {
-        private readonly NameValueCollection _settings;
+        private readonly Func<NameValueCollection> _getSettings;
 
-        public WebConfigSettingsProvider() : this(WebConfigurationManager.AppSettings)
+        public WebConfigSettingsProvider()
         {
+            _getSettings = () => WebConfigurationManager.AppSettings;
         }
 
         public WebConfigSettingsProvider(NameValueCollection settings)
         {
-            _settings = settings;
+            _getSettings = () => settings;
         }
 
         public bool GetBoolSetting(string key, bool defaultValue)
         {
+            var settings = _getSettings();
             bool value;
-            return !bool.TryParse(_settings[key], out value) ? defaultValue : value;
+            return !bool.TryParse(settings[key], out value) ? defaultValue : value;
         }
     }
 }
