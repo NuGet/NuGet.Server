9b439f2af97cb48fd9aa9eec5f1b4688211b8a29
Joel Verhagen <joel.verhagen@gmail.com>
2017-03-10 16:32:00 -0800

    Use ServerCommon build scripts for build, test, and signing (#34)
    * Move to ServerCommon build scripts

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -13,7 +13,4 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.14.0.0")]
-[assembly: AssemblyFileVersion("2.14.0.0")]
-
 [assembly: NeutralResourcesLanguage("en-US")]
