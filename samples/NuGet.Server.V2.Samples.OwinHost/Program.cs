44ce5767383e048bd83605d7af3d810aa32a6d93
Tarjei Olsen <tarjei@b-kode.no>
2018-02-03 18:27:44 +0100

    Configurable cache file name (#49)
    * Added support for custom cache file name.
    
    Handy when you have configured two feeds pointing to the same packages
    folder.
    
    * added some air after commas in type parameter
    
    * added curlies, even on one liners
    
    * renamed misleading InitializeServerPackageStore method to InitializeServerPackageCache
    
    * Updated XML comment explaining the custom cache file name option
    
    * Throwing up on invalid cache file name. Added tests to prove it.
    
    * compacting test arrange code

--- samples/NuGet.Server.V2.Samples.OwinHost/Program.cs
+++ samples/NuGet.Server.V2.Samples.OwinHost/Program.cs
@@ -24,7 +24,7 @@ namespace NuGet.Server.V2.Samples.OwinHost
 
             // Set up a common settingsProvider to be used by all repositories. 
             // If a setting is not present in dictionary default value will be used.
-            var settings = new Dictionary<string, bool>();
+            var settings = new Dictionary<string, object>();
             settings.Add("enableDelisting", false);                         //default=false
             settings.Add("enableFrameworkFiltering", false);                //default=false
             settings.Add("ignoreSymbolsPackages", true);                    //default=false
